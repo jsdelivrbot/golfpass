96575dc0b50622a6bbf052ce15f2eb1651146ec7
santutu <santutu@naver.com>
2017-10-31 11:13:41 +0900

    create test controller and commit test

---
+++ application/modules/test/controllers/Test.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Test extends CI_Controller
+// class Sample extends Public_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+      
+        echo "test";
+    }
+    function index()
+    {
+
+    }
+
+}
