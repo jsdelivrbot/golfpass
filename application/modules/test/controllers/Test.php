a9d9ad3ce55d3d1abacdc449a0860882c070c31b
santutu <santutu@naver.com>
2017-10-31 14:38:03 +0900

    a little updatied count_day_func

--- application/modules/test/controllers/Test.php
+++ application/modules/test/controllers/Test.php
@@ -48,8 +48,8 @@ class Test extends Public_Controller
 
         echo "+{$interval->days} 일:".date("Y-m-d H:i:s",strtotime ("+{$interval->days} days"));
 
-        $startDate = '2017-10-31'; // 오늘 날짜입니다.
-        $targetDate = '2017-11-14'; // 타겟 날짜를 지정합니다.
+        $startDate = '2017-11-31'; // 오늘 날짜입니다.
+        $targetDate = '2017-12-14'; // 타겟 날짜를 지정합니다.
         // $startDate = '2016-02-01'; // 오늘 날짜입니다.
         // $targetDate = '2017-02-01'; // 타겟 날짜를 지정합니다.
         $interval = date_diff(date_create($startDate), date_create($targetDate),true);
@@ -66,14 +66,14 @@ class Test extends Public_Controller
 
         echo "<br>";
         echo "<br>";
-        echo "워킹데이: ".$num_working_days=$this->number_of_days($startDate, $targetDate)->num_workingdays;
+        $obj_num_days =$this->number_of_days($startDate, $targetDate);
+        echo "워킹데이: ".$num_working_days=$obj_num_days->num_workingdays;
         echo "<br>";
-        
-        echo "주말: ".$num_weekendday_days=$this->number_of_days($startDate, $targetDate)->num_weekenddays;
+        echo "주말: ".$num_weekend_days=$obj_num_days->num_weekenddays;
         echo "<br>";
-        echo "공휴일: ".$num_holiday_days=$this->number_of_days($startDate, $targetDate)->num_holidays;
+        echo "공휴일: ".$num_holidays=$obj_num_days->num_holidays;
         echo "<br>";
-        echo "합: ".($num_weekend_days + $num_working_days+$num_holiday_days);
+        echo "합: ".$obj_num_days->num_days;
         // var_dump($num_working_days);
         // var_dump($num_weekend_days);
         
@@ -84,9 +84,8 @@ class Test extends Public_Controller
     function number_of_days($from, $to) {
         $workingDays = [1, 2, 3, 4, 5]; # date format = N (1 = Monday, ...)
         $weekendDays = [6,7]; # date format = N (1 = Monday, ...)
-        // $holidayDays = ['*-12-25', '*-01-01', '2013-12-23']; # variable and fixed holidays
         $holidayDays = ['*-12-03']; # variable and fixed holidays
-        // $holidayDays = []; # variable and fixed holidays
+        // $holidayDays = ['*-12-25', '*-01-01', '2013-12-23']; # variable and fixed holidays
     
         $from = new DateTime($from);
         $to = new DateTime($to);
@@ -98,9 +97,6 @@ class Test extends Public_Controller
         $num_holidays =0;
         $num_weekenddays=0;
         foreach ($periods as $period) {
-            // if (!in_array($period->format('N'), $workingDays)) continue;
-            // if (in_array($period->format('Y-m-d'), $holidayDays)) continue;
-            // if (in_array($period->format('*-m-d'), $holidayDays)) continue;
             //날짜 카운팅(공휴일, 주말 제외)
             if (in_array($period->format('N'), $workingDays) && !in_array($period->format('*-m-d'), $holidayDays) && !in_array($period->format('Y-m-d'), $holidayDays))
             {
@@ -111,16 +107,15 @@ class Test extends Public_Controller
             {
                $num_weekenddays++;
             }
-        
             //공휴일 카운팅
             if (in_array($period->format('*-m-d'), $holidayDays)||in_array($period->format('*-m-d'), $holidayDays))
             {
                 $num_holidays++;
             }
-
         }
-        
-        return (object)array('num_holidays'=>$num_holidays,'num_workingdays'=>$num_workingdays,'num_weekenddays'=>$num_weekenddays);
+        //전체일수
+        $num_days = $num_workingdays + $num_holidays +$num_weekenddays;
+        return (object)array('num_holidays'=>$num_holidays,'num_workingdays'=>$num_workingdays,'num_weekenddays'=>$num_weekenddays,'num_days'=>$num_days);
     }
     function number_of_working_days($from, $to) {
         $workingDays = [1, 2, 3, 4, 5]; # date format = N (1 = Monday, ...)
