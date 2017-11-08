78e1a0a1ae70db428d193dd28797c0f37e85cc37
kimmincastle <koreakms0810@gmail.com>
2017-11-08 16:56:05 +0900

    etc

--- application/modules/publishing/views/login.php
+++ application/modules/publishing/views/login.php
@@ -52,7 +52,7 @@
 
             <div class="ui divider"></div>
             <div class="">
-                <a href="">회원가입</a> | <a href="">비밀번호 찾기</a> | <a href="">아이디 찾기</a>
+                <a href="">아이디 찾기</a> | <a href="">비밀번호 찾기</a> | <a href="">회원가입</a>
             </div>
         </form>
     </div>
