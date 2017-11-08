8e351cb10c5ee4cba19a44e93f8cb4a4e895ba4b
kimmincastle <koreakms0810@gmail.com>
2017-11-08 14:19:48 +0900

    margin top

--- application/modules/publishing/views/forgot_id.php
+++ application/modules/publishing/views/forgot_id.php
@@ -1,5 +1,1 @@
-
-<link rel="stylesheet/less" type="text/css" href="/public/framework/semantic/src/semantic.less">
-<script src="/public/framework/semantic/src/less.min.js"></script>
-<div style="margin-top:120px;"></div>
-
+<div style="margin-top:180px;"></div>
