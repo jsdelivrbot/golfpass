8fd00cc1caf4e32e3a892d9054391bf8d7d5cedf
kimmincastle <koreakms0810@gmail.com>
2017-11-10 15:35:41 +0900

    all_ranking

--- application/modules/shop/views/product/golfpass/gets_by_ranking.php
+++ application/modules/shop/views/product/golfpass/gets_by_ranking.php
@@ -1,5 +1,174 @@
+<div id="panel-wrap">
+    <article id="panel-article" class="container-fluid">
+        <div class="row text-center">
+            <div class="col-12">
+                <a href="#">
+                    <h1 id='panel-title' class="text-center"><span>순위별</span> 골프장</h1>
+                </a>
+            </div>
+        </div>
+    </article>
+<div id="main-wrap">
+    <section id="section5" class="mb-5  container-fluid align-items-start align-items-md-center">
+        <article class="w-100 p-xl-4" style="width:80% !important; margin: 0 auto;">
+            <div class="d-flex mb-5 category flex-wrap">
+                <button class="btn btn-outline-light btn-sm active">#평점이 높은 코스</button>
+                <button class="btn btn-outline-light btn-sm">#전략성이 요구되는 코스</button>
+                <button class="btn btn-outline-light btn-sm">#식사가 맛있는 코스</button>
+                <button class="btn btn-outline-light btn-sm">#가성비 좋은 코스</button>
+                <button class="btn btn-outline-light btn-sm">#시설이 화려한 코스</button>
+            </div>
+            <div class="row no-gutters">
+                <div class="col-12 col-lg-12">
+                    <!--1위부터 3위까지 아래 div.content-box-->
+                    <div class="col-12 content-box">
+                        <a href="http://golfpass.net/index.php/shop/product/get/3">
+                            <div class="d-flex align-items-center p-4 mb-3 content" style="height: 150px; background-image: url(/image/rank_001.png)">
+                                <div class='d-flex align-items-center justify-content-center bg-light rounded-circle'>
+                                    <span class="d-flex align-items-center justify-content-center">1</span>
+                                </div>
+                                <div class="d-flex flex-column ml-4 text-light">
+                                    <h1>니조 컨트리 클럽</h1>
+                                    <p class="mb-0">Nijo Country Club - 일본, 후쿠오카</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-12 content-box">
+                        <a href="http://golfpass.net/index.php/shop/product/get/4">
+                            <div class="d-flex align-items-center p-4 mb-3 content" style="height: 150px; background-image: url(/image/rank_002.png)">
+                                <div class='d-flex align-items-center justify-content-center bg-light rounded-circle'>
+                                    <span class="d-flex align-items-center justify-content-center">2</span>
+                                </div>
+                                <div class="d-flex flex-column ml-4 text-light">
+                                    <h1>센트럴 후쿠오카 골프 클럽</h1>
+                                    <p class="mb-0">Central Fukuoka Golf Club - 일본, 후쿠오카</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-12 content-box">
+                        <a href="http://golfpass.net/index.php/shop/product/get/5">
+                            <div class="d-flex align-items-center p-4 mb-3 content" style="height: 150px; background-image: url(/image/rank_003.png)">
+                                <div class='d-flex align-items-center justify-content-center bg-light rounded-circle'>
+                                    <span class="d-flex align-items-center justify-content-center">3</span>
+                                </div>
+                                <div class="d-flex flex-column ml-4 text-light">
+                                    <h1>카호 골프 클럽</h1>
+                                    <p class="mb-0">Kaho Golf Club - 일본, 후쿠오카</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
 
+                </div>
+                <div class="col-12 col-lg-12">
+                    <ul class="list-unstyled">
+                        <!--4위부터 ~ li반복 -->
+                        <li class='p-3 text-light list-after-four'>
+                            <a href="http://golfpass.net/index.php/shop/product/get/6">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <p class='mb-0' style="color:#333;"><span class='mr-3 ml-2'>4</span> 후쿠오카 페잔트 컨트리 클럽</p>
+                                    </div>
+                                    <div>
+                                        <span>일본, 후쿠오카</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class='p-3 text-light list-after-four'>
+                            <a href="http://golfpass.net/index.php/shop/product/get/7">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <p class='mb-0' style="color:#333;"><span class='mr-3 ml-2'>5</span> 하우스텐보스 컨트리 클럽</p>
+                                    </div>
+                                    <div>
+                                        <span>일본, 나가시키</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class='p-3 text-light list-after-four'>
+                            <a href="http://golfpass.net/index.php/shop/product/get/8">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <p class='mb-0' style="color:#333;"><span class='mr-3 ml-2'>6</span> 나가사키 파크 컨트리 클럽</p>
+                                    </div>
+                                    <div>
+                                        <span>일본, 나가시키</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class='p-3 text-light list-after-four'>
+                            <a href="http://golfpass.net/index.php/shop/product/get/9">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <p class='mb-0' style="color:#333;"><span class='mr-3 ml-2'>7</span> 사세보코쿠사이 컨트리 클럽</p>
+                                    </div>
+                                    <div>
+                                        <span>일본, 나가시키</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class='p-3 text-light list-after-four'>
+                            <a href="http://golfpass.net/index.php/shop/product/get/10">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <p class='mb-0' style="color:#333;"><span class='mr-3 ml-2'>8</span> 아마가세 온센 컨트리 클럽</p>
+                                    </div>
+                                    <div>
+                                        <span>일본, 오이타</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class='p-3 text-light list-after-four'>
+                            <a href="http://golfpass.net/index.php/shop/product/get/11">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <p class='mb-0' style="color:#333;"><span class='mr-3 ml-2'>9</span> 벳부노모리 골프 클럽</p>
+                                    </div>
+                                    <div>
+                                        <span>일본, 오이타</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li class='p-3 text-light list-after-four'>
+                            <a href="http://golfpass.net/index.php/shop/product/get/12">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <p class='mb-0' style="color:#333;"><span class='mr-3 ml-2'>10</span> 키쿠치 컨트리 클럽</p>
+                                    </div>
+                                    <div>
+                                        <span>일본, 쿠마모토</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+    </section>
+</div>
 
-<div stlye="margin-top: 150px;"></div>
-
-순위표 리스트 샘플 페이지입니다.
+<script src="/public/sangmin/js/jquery-3.2.1.min.js"></script>
+<script>
+	$('#jssor_1').width($('#section2').width()).children('div').width($('#section2').width());
+	$(window).resize(function () {
+		$('#jssor_1').width($('#section2').width()).children('div').width($('#section2').width());
+	});
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+		integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+		crossorigin="anonymous"></script>
+<script src="/public/sangmin/dist/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="/public/sangmin/js/jssor.slider-26.5.0.min.js"></script>
+<script src="public/sangmin/js/custom/main.js"></script>
+<script src="public/sangmin/js/custom/navAction.js"></script>
+<script src="public/sangmin/js/main_section2.js"></script>
+</div>
