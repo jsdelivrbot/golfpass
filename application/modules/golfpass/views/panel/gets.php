6dcfb49c983d2464ccf53357600e9f796c5dfe4b
kimmincastle <koreakms0810@gmail.com>
2017-11-14 11:53:13 +0900

    panel

--- application/modules/golfpass/views/panel/gets.php
+++ application/modules/golfpass/views/panel/gets.php
@@ -83,7 +83,9 @@
 								<div class="col-12 d-flex justify-content-center align-items-center pagination" style="padding:0;">
 							    	<?php echo $this->ajax_pgi_2->create_links(); ?>
 								</div>
-								<a href="<?=site_url(content_uri."/add?board_id=1")?>">글쓰기</a>
+								<div class="post_write">
+								    <a href="<?=site_url(content_uri."/add?board_id=1")?>">글쓰기</a>
+								</div>
 								<!-- 페이지네이션 샘플 -->
 							 <!-- <div class="col-12 d-flex justify-content-center align-items-center pagination">
 									 <div class="prev">
