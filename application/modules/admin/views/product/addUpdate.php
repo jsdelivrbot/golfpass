24059b80ead5e1ee11bc8d656558743d41c04167
santutu <santutu@naver.com>
2017-11-17 01:24:16 +0900

    상품옵션 클릭하게 편하게 ux

--- application/modules/admin/views/product/addUpdate.php
+++ application/modules/admin/views/product/addUpdate.php
@@ -350,32 +350,22 @@ $('#navi_btn').click(function(){
     <i class="plus icon"></i>
     상품옵션
     </h1>
+
+    <div class="inline field">
+        </div>
     <form class="ui form"   onsubmit="ajax_submit(this); return false;"  action="<?=my_site_url(admin_product_uri."/ajax_options_reset")?>" method ="post">
-    <input type="checkbox" name="option[]" value="한국스태프" <?=my_set_checked_arr($options,'name','한국스태프')?>> 한국스태프
-    <input type="checkbox" name="option[]" value="와이파이(로비)" <?=my_set_checked_arr($options,'name','와이파이(로비)')?>>와이파이(로비)
-    <input type="checkbox" name="option[]" value="와이파이(룸)" <?=my_set_checked_arr($options,'name','와이파이(룸)')?>>와이파이(룸)
-    <input type="checkbox" name="option[]" value="공항 셔틀 버스" <?=my_set_checked_arr($options,'name','공항 셔틀 버스')?>>공항 셔틀 버스
-    <input type="checkbox" name="option[]" value="수영장" <?=my_set_checked_arr($options,'name','수영장')?>>수영장
-    <input type="checkbox" name="option[]" value="피트니스 센터" <?=my_set_checked_arr($options,'name','피트니스 센터')?>>피트니스 센터
-    <input type="checkbox" name="option[]" value="사우나" <?=my_set_checked_arr($options,'name','사우나')?>>사우나
-    <input type="checkbox" name="option[]" value="마사지 샵" <?=my_set_checked_arr($options,'name','마사지 샵')?>>마사지 샵<br>
-    <input type="checkbox" name="option[]" value="커피숍" <?=my_set_checked_arr($options,'name','커피숍')?>>커피숍
-    <input type="checkbox" name="option[]" value="레스토랑" <?=my_set_checked_arr($options,'name','레스토랑')?>>레스토랑
-    <input type="checkbox" name="option[]" value="바" <?=my_set_checked_arr($options,'name','바')?>>바
-    <input type="checkbox" name="option[]" value="룸 서비스" <?=my_set_checked_arr($options,'name','룸 서비스')?>>룸 서비스
-    <input type="checkbox" name="option[]" value="+24시간 룸 서비스" <?=my_set_checked_arr($options,'name','+24시간 룸 서비스')?>>+24시간 룸 서비스
-    <input type="checkbox" name="option[]" value="24시간 체크인" <?=my_set_checked_arr($options,'name','24시간 체크인')?>>24시간 체크인
-    <input type="checkbox" name="option[]" value="레이트 체크 아웃" <?=my_set_checked_arr($options,'name','레이트 체크 아웃')?>>레이트 체크 아웃
-    <input type="checkbox" name="option[]" value="금고" <?=my_set_checked_arr($options,'name','금고')?>>금고
-    <input type="checkbox" name="option[]" value="헤어 드라이기" <?=my_set_checked_arr($options,'name','헤어 드라이기')?>>헤어 드라이기<br>
-    <input type="checkbox" name="option[]" value="비데" <?=my_set_checked_arr($options,'name','비데')?>>비데
-    <input type="checkbox" name="option[]" value="생수" <?=my_set_checked_arr($options,'name','생수')?>>생수
-    <input type="checkbox" name="option[]" value="노래방" <?=my_set_checked_arr($options,'name','노래방')?>>노래방
-    <input type="checkbox" name="option[]" value="편의점" <?=my_set_checked_arr($options,'name','편의점')?>>편의점
-    <input type="checkbox" name="option[]" value="택시대기" <?=my_set_checked_arr($options,'name','택시대기')?>>택시대기
-    <input type="checkbox" name="option[]" value="환전" <?=my_set_checked_arr($options,'name','환전')?>>환전
-    <input type="checkbox" name="option[]" value="냉난방" <?=my_set_checked_arr($options,'name','냉난방')?>>냉난방
-    <input type="checkbox" name="option[]" value="흡연가능" <?=my_set_checked_arr($options,'name','흡연가능')?>>흡연가능
+        <?php 
+        $v_options=array("한국스태프","와이파이(로비)","와이파이(룸)","공항 셔틀 버스","수영장","피트니스 센터","사우나",
+        "마사지 샵","커피숍","레스토랑","바","룸 서비스","+24시간 룸 서비스","24시간 체크인"
+        ,"레이트 체크 아웃","금고","헤어 드라이기","비데","생수","노래방","편의점","편의점","택시대기","환전","냉난방",
+        "흡연가능");
+        for($i=0 ; $i < count($v_options); $i++){?>
+        <div class="ui checkbox">
+          <input type="checkbox" tabindex="0" class="hidden" name="option[]" value="<?=$v_options[$i]?>" <?=my_set_checked_arr($options,'name',$v_options[$i])?>>
+          <label><?=$v_options[$i]?></label>
+        </div>
+        <?php }?>
+    
     
     <input type="hidden" name="product_id" value="<?=$product->id?>">
     <br>
@@ -499,6 +489,9 @@ $('#navi_btn').click(function(){
 
 <script>
 $('.ui.radio.checkbox')
+  .checkbox()
+;
+$('.ui.checkbox')
   .checkbox()
 ;
 </script>
