5e14111eb65f1100d65cd4c8ea79dac8e6229863
TARDIS\korea <koreakms0810@gmail.com>
2017-11-20 02:24:33 +0900

    dd

--- application/modules/golfpass/views/panel_content/get.php
+++ application/modules/golfpass/views/panel_content/get.php
@@ -1,5 +1,3 @@
-<div style="margin-top:50px;"></div>
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/public/etc/kimmincastle/blog_detail.css">
 
@@ -34,8 +32,10 @@
 
     <!-- First Grid -->
     <div class="w3-row-padding w3-padding-64 w3-container">
-        <a href="<?=site_url(content_uri."/update/{$content->id}?board_id=1")?>">수정</a>
-        <a onclick="confirm_redirect('<?=site_url(content_uri."/delete/{$content->id}?board_id=1")?>','정말 삭제하시겠습니까? 복구 할 방법이 없습니다.')" href="#">삭제</a>
+        <div class="tp-post_write"> 
+          <a href="<?=site_url(content_uri."/update/{$content->id}?board_id=1")?>">수정</a>
+          <a onclick="confirm_redirect('<?=site_url(content_uri."/delete/{$content->id}?board_id=1")?>','정말 삭제하시겠습니까? 복구 할 방법이 없습니다.')" href="#">삭제</a>
+        </div> 
         <div class="w3-content">
             <div class="w3-twothird">
                 <h1><?=$content->title?></h1>
