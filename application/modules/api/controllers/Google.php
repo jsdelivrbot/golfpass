03ff03bcd72187933335d012115bdb3dac0bacea
santutu <santutu@naver.com>
2017-12-04 23:22:45 +0900

    d

---
+++ application/modules/api/controllers/Google.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Google extends Public_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	//Do your magic here
+	}
+	public function index()
+	{
+		
+	}
+	function redirect_url()
+	{
+		
+	}
+
+}
+
+/* End of file Google.php */
+/* Location: .//C/Users/이재윤/Desktop/01shortcut/modules/api/controllers/Google.php */
