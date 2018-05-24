7adeadf1cbb9b644661c8dda1bcb70040c614936
kimmincastle <koreakms0810@gmail.com>
2018-05-24 12:18:43 +0900

    Revert "패키지"
    This reverts commit f5b0d0f41069905ee8afbdc5dd186f2905b18a0c.

--- application/modules/shop/models/Ref_cate_package_model.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('no direct script access allrowed');
-
-class Ref_cate_package_Model extends Public_Model{
-    function __construct(){
-        parent:: __construct('ref_cate_package');
-    }
- 
-
-}
