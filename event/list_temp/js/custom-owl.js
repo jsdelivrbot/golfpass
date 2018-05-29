9935612d35da024cc3484591d16d3608bac6c524
임수빈 <sbin910@gmail.com>
2018-05-29 17:30:03 +0900

    eventpage_resort

--- event/list_temp/js/custom-owl.js
+++ event/list_temp/js/custom-owl.js
@@ -3,12 +3,7 @@
 	"use strict";
 	
 	// Cache Selectors
-	var rooms			=$("#owl-rooms"),
-		team			=$("#owl-team"),
-		spa				=$("#owl-spa-services"),
-		specials		=$("#owl-specials"),
-		dishDeal		=$("#owl-dish-deals"),
-		testimonials2	=$("#owl-testimonials-2");
+	var rooms			=$("#owl-rooms");
 	
 	// Owl Rooms
 	rooms.owlCarousel({
@@ -43,41 +38,18 @@
 	});
 	
 	
-	// Owl Team
-	team.owlCarousel({
-		items : 4,
-		itemsCustom : false,
-		itemsDesktop : [1199,4],
-		itemsDesktopSmall : [991,3],
-		itemsTablet: [768,2],
-		itemsTabletSmall: [600,2],
-		itemsMobile : [479,1],
-		singleItem : false,
-		itemsScaleUp : false,
-	 
-		//Autoplay
-		autoPlay : true,
-		stopOnHover : true,
-	 
-		// Navigation
-		navigation : false,
-		navigationText : false,
-		rewindNav : false,
-		scrollPerPage : false,
-	 
-		//Pagination
-		pagination : true,
-		paginationNumbers: false,
-	 
-		// Responsive 
-		responsive: true,
-		responsiveRefreshRate : 200,
-		responsiveBaseWidth: window,    
-	});
+})(jQuery);
+
+
+(function($) {
 	
+	"use strict";
 	
-	// Owl Team
-	spa.owlCarousel({
+	// Cache Selectors
+	var rooms			=$("#owl-rooms2");
+	
+	// Owl Rooms
+	rooms.owlCarousel({
 		items : 3,
 		itemsCustom : false,
 		itemsDesktop : [1199,3],
@@ -109,102 +81,91 @@
 	});
 	
 	
-	// Owl Specials
-	specials.owlCarousel({
-		items : 4,
-		itemsCustom : false,
-		itemsDesktop : [1199,4],
-		itemsDesktopSmall : [991,3],
-		itemsTablet: [768,3],
-		itemsTabletSmall: [600,2],
-		itemsMobile : [420,1],
-		singleItem : false,
-		itemsScaleUp : false,
-	
-		//Autoplay
-		autoPlay : true,
-		stopOnHover : true,
-	 
-		// Navigation
-		navigation : true,
-		navigationText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-		rewindNav : true,
-		scrollPerPage : false,
-	 
-		//Pagination
-		pagination : false,
-		paginationNumbers: false,
-	 
-		// Responsive 
-		responsive: true,
-		responsiveRefreshRate : 200,
-		responsiveBaseWidth: window,    
-	});
-	
-	
-	// Owl Dish Deal
-	dishDeal.owlCarousel({
-		items : 1,
-		itemsCustom : false,
-		itemsDesktop : [1199,1],
-		itemsDesktopSmall : [991,1],
-		itemsTablet: [768,1],
-		itemsTabletSmall: [600,1],
-		itemsMobile : [420,1],
-		singleItem : false,
-		itemsScaleUp : false,
-	
-		//Autoplay
-		autoPlay : true,
-		stopOnHover : true,
-	 
-		// Navigation
-		navigation : true,
-		navigationText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-		rewindNav : true,
-		scrollPerPage : false,
-	 
-		//Pagination
-		pagination : false,
-		paginationNumbers: false,
-	 
-		// Responsive 
-		responsive: true,
-		responsiveRefreshRate : 200,
-		responsiveBaseWidth: window,    
-	});
-	
-	
-	// Owl Testimonials-2
-	testimonials2.owlCarousel({
-		items : 1,
-		itemsCustom : false,
-		itemsDesktop : [1199,1],
-		itemsDesktopSmall : [991,1],
-		itemsTablet: [768,1],
-		itemsTabletSmall: [600,1],
-		itemsMobile : [420,1],
-		singleItem : false,
-		itemsScaleUp : false,
-	
-		//Autoplay
-		autoPlay : true,
-		stopOnHover : true,
-	 
-		// Navigation
-		navigation : true,
-		navigationText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-		rewindNav : true,
-		scrollPerPage : false,
-	 
-		//Pagination
-		pagination : false,
-		paginationNumbers: false,
-	 
-		// Responsive 
-		responsive: true,
-		responsiveRefreshRate : 200,
-		responsiveBaseWidth: window,    
-	});
-
 })(jQuery);
+
+
+(function($) {
+	
+	"use strict";
+	
+	// Cache Selectors
+	var rooms			=$("#owl-rooms3");
+	
+	// Owl Rooms
+	rooms.owlCarousel({
+		items : 3,
+		itemsCustom : false,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [991,2],
+		itemsTablet: [768,2],
+		itemsTabletSmall: [600,1],
+		itemsMobile : [479,1],
+		singleItem : false,
+		itemsScaleUp : false,
+	 
+		//Autoplay
+		autoPlay : true,
+		stopOnHover : true,
+	 
+		// Navigation
+		navigation : false,
+		navigationText : false,
+		rewindNav : false,
+		scrollPerPage : false,
+	 
+		//Pagination
+		pagination : true,
+		paginationNumbers: false,
+	 
+		// Responsive 
+		responsive: true,
+		responsiveRefreshRate : 200,
+		responsiveBaseWidth: window,    
+	});
+	
+	
+})(jQuery);
+
+
+(function($) {
+	
+	"use strict";
+	
+	// Cache Selectors
+	var rooms			=$("#owl-rooms4");
+	
+	// Owl Rooms
+	rooms.owlCarousel({
+		items : 3,
+		itemsCustom : false,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [991,2],
+		itemsTablet: [768,2],
+		itemsTabletSmall: [600,1],
+		itemsMobile : [479,1],
+		singleItem : false,
+		itemsScaleUp : false,
+	 
+		//Autoplay
+		autoPlay : true,
+		stopOnHover : true,
+	 
+		// Navigation
+		navigation : false,
+		navigationText : false,
+		rewindNav : false,
+		scrollPerPage : false,
+        
+		//Pagination
+		pagination : true,
+		paginationNumbers: false,
+	 
+		// Responsive 
+		responsive: true,
+		responsiveRefreshRate : 200,
+		responsiveBaseWidth: window,    
+	});
+	
+	
+})(jQuery);
+
