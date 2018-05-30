df4e7b018d77bb8553fc3151f8c636bc2419d06a
임수빈 <sbin910@gmail.com>
2018-05-30 10:57:36 +0900

    별점이랑 파일위치 변경

---
+++ public/event/list_temp/js/custom-owl.js
@@ -0,0 +1,171 @@
+(function($) {
+	
+	"use strict";
+	
+	// Cache Selectors
+	var rooms			=$("#owl-rooms");
+	
+	// Owl Rooms
+	rooms.owlCarousel({
+		items : 3,
+		itemsCustom : false,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [991,2],
+		itemsTablet: [768,2],
+		itemsTabletSmall: [600,1],
+		itemsMobile : [479,1],
+		singleItem : false,
+		itemsScaleUp : false,
+	 
+		//Autoplay
+		autoPlay : true,
+		stopOnHover : true,
+	 
+		// Navigation
+		navigation : false,
+		navigationText : false,
+		rewindNav : false,
+		scrollPerPage : false,
+	 
+		//Pagination
+		pagination : true,
+		paginationNumbers: false,
+	 
+		// Responsive 
+		responsive: true,
+		responsiveRefreshRate : 200,
+		responsiveBaseWidth: window,    
+	});
+	
+	
+})(jQuery);
+
+
+(function($) {
+	
+	"use strict";
+	
+	// Cache Selectors
+	var rooms			=$("#owl-rooms2");
+	
+	// Owl Rooms
+	rooms.owlCarousel({
+		items : 3,
+		itemsCustom : false,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [991,2],
+		itemsTablet: [768,2],
+		itemsTabletSmall: [600,1],
+		itemsMobile : [479,1],
+		singleItem : false,
+		itemsScaleUp : false,
+	 
+		//Autoplay
+		autoPlay : true,
+		stopOnHover : true,
+	 
+		// Navigation
+		navigation : false,
+		navigationText : false,
+		rewindNav : false,
+		scrollPerPage : false,
+	 
+		//Pagination
+		pagination : true,
+		paginationNumbers: false,
+	 
+		// Responsive 
+		responsive: true,
+		responsiveRefreshRate : 200,
+		responsiveBaseWidth: window,    
+	});
+	
+	
+})(jQuery);
+
+
+(function($) {
+	
+	"use strict";
+	
+	// Cache Selectors
+	var rooms			=$("#owl-rooms3");
+	
+	// Owl Rooms
+	rooms.owlCarousel({
+		items : 3,
+		itemsCustom : false,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [991,2],
+		itemsTablet: [768,2],
+		itemsTabletSmall: [600,1],
+		itemsMobile : [479,1],
+		singleItem : false,
+		itemsScaleUp : false,
+	 
+		//Autoplay
+		autoPlay : true,
+		stopOnHover : true,
+	 
+		// Navigation
+		navigation : false,
+		navigationText : false,
+		rewindNav : false,
+		scrollPerPage : false,
+	 
+		//Pagination
+		pagination : true,
+		paginationNumbers: false,
+	 
+		// Responsive 
+		responsive: true,
+		responsiveRefreshRate : 200,
+		responsiveBaseWidth: window,    
+	});
+	
+	
+})(jQuery);
+
+
+(function($) {
+	
+	"use strict";
+	
+	// Cache Selectors
+	var rooms			=$("#owl-rooms4");
+	
+	// Owl Rooms
+	rooms.owlCarousel({
+		items : 3,
+		itemsCustom : false,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [991,2],
+		itemsTablet: [768,2],
+		itemsTabletSmall: [600,1],
+		itemsMobile : [479,1],
+		singleItem : false,
+		itemsScaleUp : false,
+	 
+		//Autoplay
+		autoPlay : true,
+		stopOnHover : true,
+	 
+		// Navigation
+		navigation : false,
+		navigationText : false,
+		rewindNav : false,
+		scrollPerPage : false,
+        
+		//Pagination
+		pagination : true,
+		paginationNumbers: false,
+	 
+		// Responsive 
+		responsive: true,
+		responsiveRefreshRate : 200,
+		responsiveBaseWidth: window,    
+	});
+	
+	
+})(jQuery);
+
